20b30497fa8c4e7a672916ce09367d98bf899aca
Valera <valerakaktotak@gmail.com>
2024-07-04 20:41:26 +0300

    created header and footer empty components

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+
+interface IFooter {}
+
+const Footer: FC<IFooter> = ({}) => {
+	return (
+		<div>Footer</div>
+	)
+}
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+
+interface IHeader {}
+
+const Header: FC<IHeader> = ({}) => {
+	return (
+		<div>Header</div>
+	)
+}
+export default Header
