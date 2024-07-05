9b0a379604e7286bff3dec21ad575a130e1675d6
Valera <valerakaktotak@gmail.com>
2024-07-05 08:42:48 +0300

    added ThemeProvider and fix prettier save formatter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,6 @@ import type { FC } from 'react'
 interface IFooter {}
 
 const Footer: FC<IFooter> = ({}) => {
-	return (
-		<div>Footer</div>
-	)
+  return <div>Footer</div>
 }
 export default Footer
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
+'use client'
 import type { FC } from 'react'
+import ThemeToggler from './ThemeToggler'
 
 interface IHeader {}
 
 const Header: FC<IHeader> = ({}) => {
-	return (
-		<div>Header</div>
-	)
+  return (
+    <div>
+      <ThemeToggler />
+    </div>
+  )
 }
 export default Header
