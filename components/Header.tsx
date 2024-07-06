b33a0b5b1dd90020fda512aa52db1d1a815704a0
Valera <valerakaktotak@gmail.com>
2024-07-06 13:21:27 +0300

    done header component main styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
-import type { FC } from 'react'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState, type FC } from 'react'
 
 //Components
 import Logo from '@/components/Logo'
@@ -10,8 +11,26 @@ import ThemeToggler from '@/components/ThemeToggler'
 interface IHeader {}
 
 const Header: FC<IHeader> = ({}) => {
+  const [header, setHeader] = useState(false)
+  const path = usePathname()
+
+  useEffect(() => {
+    const scrollYPos = () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false)
+    }
+    window.addEventListener('scroll', scrollYPos)
+
+    return () => window.removeEventListener('scroll', scrollYPos)
+  }, [])
+
   return (
-    <header>
+    <header
+      className={`${
+        header
+          ? 'bg-tertiary py-4 shadow-lg dark:bg-accent'
+          : 'py-6 dark:bg-transparent'
+      } sticky top-0 transition-all ${path === '/' && 'bg-[#fff]'}`}
+    >
       <div className='container'>
         <div className='flex items-center justify-between'>
           {/*Logo Section */}
--- hooks/useScrollProgress.ts
+++ hooks/useScrollProgress.ts
@@ -1,8 +1,6 @@
-import { useEffect, useState, type FC } from 'react'
+import { useEffect, useState } from 'react'
 
-interface IUseScrollProgress {}
-
-const useScrollProgress: FC<IUseScrollProgress> = ({}) => {
+const useScrollProgress = () => {
   const [completion, setCompletion] = useState<number>(0)
 
   useEffect(() => {
