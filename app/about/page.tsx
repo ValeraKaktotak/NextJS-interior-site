12817e96d7e3387fed04a40b1e33611782de4d52
Valera <valerakaktotak@gmail.com>
2024-07-11 14:29:37 +0300

    done About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,27 @@
+'use client'
+
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { ArrowUpRight } from 'lucide-react'
 import Image from 'next/image'
-import type { FC } from 'react'
+import { useRef, type FC } from 'react'
 
 //Components
 import { Button } from '@/components/ui/button'
-import { ArrowUpRight } from 'lucide-react'
+
+//Utils
+import { titleVariants } from '@/utils/animation'
 
 interface Ipage {}
 
-const page: FC<Ipage> = ({}) => {
+const Page: FC<Ipage> = ({}) => {
+  const myRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: myRef,
+    offset: ['start end', 'end end']
+  })
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.6, 1])
   return (
     <div>
       <div className={`bg-[url('/image/whoarewe.jpg')] bg-cover bg-center`}>
@@ -18,32 +32,48 @@ const page: FC<Ipage> = ({}) => {
 
       <section className='container'>
         <div className='pt-4'>
-          <h2 className='text-center text-3xl font-semibold lg:p-10 lg:text-5xl'>
+          <motion.h2
+            initial='offscreen'
+            whileInView='onscreen'
+            variants={titleVariants}
+            className='text-center text-3xl font-semibold lg:p-10 lg:text-5xl'
+          >
             We have great idea & Interior Design
-          </h2>
-          <p className='mt-5 pb-10 text-center text-2xl font-medium'>
+          </motion.h2>
+
+          <motion.p
+            initial='offscreen'
+            whileInView='onscreen'
+            variants={titleVariants}
+            className='mt-5 pb-10 text-center text-2xl font-medium'
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
             pariatur molestiae iste. Tempore cum, ut reiciendis dicta veniam,
             aliquam adipisci dignissimos, vero numquam quam labore laborum vitae
             illo enim vel.
-          </p>
+          </motion.p>
         </div>
 
         <section className='items-center gap-x-8 lg:flex'>
           {/*Left Image Section */}
-          <section className='w-full text-center'>
+          <motion.section style={{ scale }} ref={myRef} className='w-full'>
             <Image
               src='/image/gallery1123.jpg'
               alt='whoarewe'
               width={700}
               height={700}
               priority
-              className='inline-flex'
+              className='mx-auto'
             />
-          </section>
+          </motion.section>
 
           {/*Right Content Section */}
-          <section className='w-full'>
+          <motion.section
+            initial='offscreen'
+            whileInView='onscreen'
+            variants={titleVariants}
+            className='w-full'
+          >
             <p className='mt-6 pb-8 tracking-wide'>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
               nulla, consequuntur, possimus fugiat reprehenderit quod ab minus
@@ -69,19 +99,107 @@ const page: FC<Ipage> = ({}) => {
             <Button className='inline-flex items-center rounded-full px-8 py-3 shadow-lg'>
               Read More <ArrowUpRight className='ml-2 h-5 w-5' />
             </Button>
-          </section>
+          </motion.section>
         </section>
 
         {/*Team Section */}
         <section className='lg:py-20'>
           <div className='pb-4 pt-8'>
-            <h1 className='text-center text-4xl font-bold uppercase tracking-wider'>
+            <motion.h1
+              initial='offscreen'
+              whileInView='onscreen'
+              variants={titleVariants}
+              className='text-center text-4xl font-bold uppercase tracking-wider'
+            >
               Team
-            </h1>
+            </motion.h1>
           </div>
+
+          <section className='grid gap-20 py-8 sm:grid-cols-3 sm:gap-4'>
+            <motion.article
+              initial='offscreen'
+              whileInView='onscreen'
+              variants={titleVariants}
+              className='border-2 border-primary'
+            >
+              <div className='-m-0.5 aspect-square bg-gray-100 p-4 text-center transition hover:-translate-y-3 hover:translate-x-3 dark:bg-tertiary'>
+                <Image
+                  src='/image/profile2.jpg'
+                  alt='profile'
+                  width={200}
+                  height={200}
+                  className='mx-auto rounded-full'
+                />
+                <h2 className='py-4 text-2xl font-semibold uppercase'>
+                  Building Surveys
+                </h2>
+                <p className='text-sm sm:line-clamp-5'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Doloremque, maiores labore! Autem consectetur quo voluptas
+                  incidunt sed! Temporibus laudantium, quidem facere dignissimos
+                  alias perspiciatis possimus impedit voluptate iusto assumenda
+                  sequi!
+                </p>
+              </div>
+            </motion.article>
+
+            <motion.article
+              initial='offscreen'
+              whileInView='onscreen'
+              variants={titleVariants}
+              className='border-2 border-primary'
+            >
+              <div className='-m-0.5 aspect-square bg-gray-100 p-4 text-center transition hover:-translate-y-3 hover:translate-x-3 dark:bg-tertiary'>
+                <Image
+                  src='/image/profile1.jpg'
+                  alt='profile'
+                  width={200}
+                  height={200}
+                  className='mx-auto rounded-full'
+                />
+                <h2 className='py-4 text-2xl font-semibold uppercase'>
+                  Building Surveys
+                </h2>
+                <p className='text-sm sm:line-clamp-5'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Doloremque, maiores labore! Autem consectetur quo voluptas
+                  incidunt sed! Temporibus laudantium, quidem facere dignissimos
+                  alias perspiciatis possimus impedit voluptate iusto assumenda
+                  sequi!
+                </p>
+              </div>
+            </motion.article>
+
+            <motion.article
+              initial='offscreen'
+              whileInView='onscreen'
+              variants={titleVariants}
+              className='border-2 border-primary'
+            >
+              <div className='-m-0.5 aspect-square bg-gray-100 p-4 text-center transition hover:-translate-y-3 hover:translate-x-3 dark:bg-tertiary'>
+                <Image
+                  src='/image/profile3.jpg'
+                  alt='profile'
+                  width={200}
+                  height={200}
+                  className='mx-auto rounded-full'
+                />
+                <h2 className='py-4 text-2xl font-semibold uppercase'>
+                  Building Surveys
+                </h2>
+                <p className='text-sm sm:line-clamp-5'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Doloremque, maiores labore! Autem consectetur quo voluptas
+                  incidunt sed! Temporibus laudantium, quidem facere dignissimos
+                  alias perspiciatis possimus impedit voluptate iusto assumenda
+                  sequi!
+                </p>
+              </div>
+            </motion.article>
+          </section>
         </section>
       </section>
     </div>
   )
 }
-export default page
+export default Page
